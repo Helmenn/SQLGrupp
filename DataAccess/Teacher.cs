7a84bf4fd0a8cd3ac05f463a323306e64ebd1cfe
Viktor Hjelm <viktor.hjelm@learnet.se>
2019-10-07 13:16:50 +0200

    Lade till allt nytt med interface å grejjs

---
+++ DataAccess/Teacher.cs
@@ -0,0 +1,13 @@
+﻿using DataInterface;
+using System;
+
+namespace DataAccess
+{
+    public class Teacher : ITeacher
+    {
+        public void AddTeacher(string teacherName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
