dfb56473e3e5bc32c8faf194b72d784166c1f8c2
Sarra Avzina <sarra.avzina@learnet.se>
2019-10-08 10:08:10 +0200

    Manger

---
+++ DataInterface/IProvManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataInterface
+{
+    public interface IProvManager
+    {
+        public void AddProv(int Andel, int CourseID);
+    }
+}
---
+++ DataInterface/IStudentManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataInterface
+{
+   public interface IStudentManager
+    {
+        public void AddStudent(string Name, int CourseID);
+    }
+}
