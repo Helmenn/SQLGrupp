d821747f6f9dba30153be5512e234ce10330de0b
Rasmus Lindberg <ragge00@hotmail.se>
2019-10-08 10:31:51 +0200

    GradesManager,IGradesManager och en mapp för tabeller

---
+++ DataAccess/GradesManager.cs
@@ -0,0 +1,22 @@
+﻿using SQL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess
+{
+    public class GradesManager
+    {
+        public void AddGrade(string gradeName, int courseID, int studentID)
+        {
+            using (var schoolContext = new SchoolContext())
+            {
+                var grade = new Grades();
+                grade.GradesName = gradeName;
+                grade.CourseID = courseID;
+                grade.StudentID = studentID;
+                schoolContext.Grades.Add(grade);
+            }
+        }
+    }
+}
---
+++ DataInterface/IGradesManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataInterface
+{
+    public interface IGradesManager
+    {
+        public void AddGrade(string gradeName, int courseID, int studentID);
+    }
+}
