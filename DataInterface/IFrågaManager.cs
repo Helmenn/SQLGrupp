9b3ddf5cfc0207e430e48a8f53728817fa567306
Viktor Hjelm <viktor.hjelm@learnet.se>
2019-10-09 09:54:00 +0200

    la till interface till frågamanager å

---
+++ DataInterface/IFrågaManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataInterface
+{
+    public interface IFrågaManager
+    {
+        public void AddFråga(string text, int provID);
+    }
+}
