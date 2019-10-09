b947b77d04a5c05e467eebad201991e373dcbb79
Viktor Hjelm <viktor.hjelm@learnet.se>
2019-10-09 09:42:42 +0200

    la till FrågaManager

---
+++ DataAccess/FrågaManager.cs
@@ -0,0 +1,21 @@
+﻿using SQL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess
+{
+    public class FrågaManager
+    {
+        public void AddFråga(string text, int provID)
+        {
+            using (var schoolContext = new SchoolContext())
+            {
+                var fråga = new Fråga();
+                fråga.FrågText = text;
+                fråga.ProvID = provID;
+                schoolContext.Fråga.Add(fråga);
+            }
+        }
+    }
+}
