ce1abcb5a84ef08b463ceaa4bc64204eecfd736a
Tobias Englund <tobias.englund@learnet.se>
2019-10-10 10:17:20 +0200

    Added comments, both regular and some XML on ProvManager.

--- DataAccess/CourseManager.cs
+++ DataAccess/CourseManager.cs
@@ -35,12 +35,29 @@ namespace DataAccess
             }
         }
         public Courses GetCourseByName(string name)
-        {
+        {   // Denna metod letar reda på en kurs som heter "name"
+            // Finns den, så får vi tillbaka objektet.
+            // Finns den inte, så får vi tillbaka null.
+            // SELECT * FROM Courses WHERE Courses.CourseName = name;
             using (var schoolContext = new SchoolContext())
             {
+                // Variabelnamnet _course är en konvention som är föreslagen av Microsoft.
+                // _ betyder en lokal variabel inom metoden.
                 var course = from _course in schoolContext.Courses
-                              where string.Compare(_course.CourseName , name, StringComparison.InvariantCulture) ==0
+                              where string.Compare(_course.CourseName , name, 
+                              StringComparison.InvariantCulture) == 0
                               select _course;
+                // string.Compare är överkurs och har att göra med att om programmet hanterar data
+                // i en viss teckentabell, som Latin1 eller ASCII och databasen har en annan teckentabell,
+                // så kan du inte jämföra strängarna direkt med varandra, som 
+                // _course.CourseName == name.
+                // Om databasen och programmet "pratar" olika teckentabeller, så kommer
+                // jämförelsen ovan inte fungera, utan vi behöver använda en mer generell metod.
+                // Metoden returnerar 0 om båda är samma, -1 eller +1 beroende på på något som jag inte minns.
+                // Vi kollar om två stängar är lika och är det de, så returnerar string.Compare(...) noll.
+                // Alltså har vi hittat en kurs i databasen som heter samma sak som strängen "name".
+
+                // Vi returnerar ett objekt till anropande program.
                 return course.First();
             }
         }
--- DataAccess/ProvManager.cs
+++ DataAccess/ProvManager.cs
@@ -11,12 +11,19 @@ namespace DataAccess
 
     {
         public Prov AddProv(int Andel, string CourseName)
-        {
+        { // Vi får in en andel i procent. Detta bestämmer hur viktigt provet är.
+            // Sedan blir det ett kursnamn
+            // Provet har inget eget namn,
+            // Vi skapar provet och returnerar sedan en länk itll provet, så att vi kan
+            // skapa frågor till rätt prov.
             using (var schoolContext = new SchoolContext())
             {
                 ICourseManager courseManager = new CourseManager();
+                // Vi tar reda på om det finns en kurs som heter det vi vill lägga ett prov på.
                 var course = courseManager.GetCourseByName(CourseName);
-
+                // Finns kursen, så kan vi skapa provet och returnera provobjektet
+                // till anropande program, så att vi sedan kan lägga till frågor på
+                // rätt prov.
                 if (course != null)
                 {
                     var prov = new Prov();
@@ -27,10 +34,22 @@ namespace DataAccess
                     schoolContext.SaveChanges();
                     return prov;
                 }
+                // Vi returnerar null om vi inte kunde skapa ett prov.
+                // Detta ska vi ta hand om i koden i vårt anropande program, så att användaren
+                // får reda på att det inte fanns en kurs som heter det användaren trodde.
                 return null;
             }
         }
 
+
+        
+        /// <summary>
+        /// Hämtar ett provobjekt baserat på dess ID.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>
+        /// Returnerar ett provebjekt.
+        /// </returns>
         public Prov GetProv(int id)
         {
             using (var schoolContext = new SchoolContext())
